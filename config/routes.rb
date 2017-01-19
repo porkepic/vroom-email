cebdb99e8ed3d254618b5f891b51ad56ea5a398e
Julien Guimont <julien.guimont@gmail.com>
2017-01-19 15:57:20 -0500

    postmark routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "/email", to: "receive#email"
+  post "/bounce", to: "receive#bounce"
+  post "/delivery", to: "receive#delivery"
+  post "/open", to: "receive#open"
 
 end
