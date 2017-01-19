e22e1bec8f8a0dc8a3debdab9734ebe609a4b637
Julien Guimont <julien.guimont@gmail.com>
2017-01-19 14:07:00 -0500

    postmark webhooks

--- app/controllers/receive_controller.rb
+++ app/controllers/receive_controller.rb
@@ -7,6 +7,10 @@ class ReceiveController < ApplicationController
 
   http_basic_authenticate_with name: ENV["RECEIVE_USER"], password: ENV["RECEIVE_PASSWORD"]
 
+  MAP_TO_SENDGRID_EVENTS = {
+
+  }
+
   def email
     raw_json = request.raw_post
     json = JSON.parse raw_json
@@ -19,13 +23,52 @@ class ReceiveController < ApplicationController
       logger.info "[#{subdomain}] : receive '#{event["event"]}' with '#{event["object"]}', '#{event["email"]}'"
 
       next unless subdomain
-      
+
       uri = URI.parse("https://#{ENV["RECEIVE_USER"]}:#{ENV["RECEIVE_PASSWORD"]}@#{subdomain}.#{ENV["RECEIVE_DOMAIN"]}")
       Net::HTTP.post_form(uri, event)
 
     end
 
-    render json: {}    
+    render json: {}
+  end
+
+  def bounce
+    postmark("bounce")
+    render json: {}
+  end
+
+  def delivery
+    postmark("delivery")
+    render json: {}
+  end
+
+  def open
+    postmark("open")
+    render json: {}
+  end
+
+  def postmark(event)
+    raw_json = request.raw_post
+    json = JSON.parse raw_json
+    logger = Rails.logger
+
+    return unless json["Tag"]
+    tag = nil
+    begin
+      tag = JSON.parse json["Tag"]
+    rescue => e
+      return
+    end
+
+    subdomain = tag["subdomain"]
+    tag["event"] = event
+
+    logger.info "[#{subdomain}] : receive '#{event["event"]}' with '#{event["object"]}', '#{event["email"]}'"
+
+    return unless subdomain
+
+    uri = URI.parse("https://#{ENV["RECEIVE_USER"]}:#{ENV["RECEIVE_PASSWORD"]}@#{subdomain}.#{ENV["RECEIVE_DOMAIN"]}")
+    Net::HTTP.post_form(uri, tag)
   end
 
 end
