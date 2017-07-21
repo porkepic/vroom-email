898861f3a8cb7e9746b81fd5d2189b8082f2fd25
Julien Guimont <julien.guimont@gmail.com>
2017-07-20 20:51:46 -0400

    Enqueue to shoryuken

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '~> 5.1.0'
 gem 'puma'
 gem 'rack-timeout'
 gem 'rollbar'
+gem 'shoryuken'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/receive_controller.rb
+++ app/controllers/receive_controller.rb
@@ -35,17 +35,82 @@ class ReceiveController < ApplicationController
 
   def bounce
     postmark("bounce")
+    enqueue("bounce")
+
     render json: {}
   end
 
   def delivery
     postmark("delivered")
+    enqueue("delivered")
+
     render json: {}
   end
 
   def open
     postmark("open")
+    enqueue("open")
+
     render json: {}
+  end
+
+  def enqueue(event)
+    raw_json = request.raw_post
+    json = JSON.parse raw_json
+    logger = Rails.logger
+
+    return if !json["Tag"] || json["Tag"] == ""
+    tag = nil
+    begin
+      tag = JSON.parse json["Tag"].gsub(/\s|\\t/, "")
+    rescue => e
+      Rollbar.error(e)
+      return
+    end
+
+    subdomain = tag["subdomain"]
+    tag["event"] = event
+
+    return unless subdomain
+
+    begin
+      matcher = /([^\.]*)\.inc.construction/
+      queue = case subdomain
+      when subdomain.end_with?( ".local.host")
+        matcher = /([^\.]*)\.local.host/
+        "dev_default"
+      # when subdomain == "fca.inc.construction"
+      #   "fca_default"
+      # when subdomain == "durotoit.inc.construction"
+      #   "durotoit_default"
+      # when subdomain.end_with?( ".inc.services")
+      #   matcher = /([^\.]*)\.inc.services/
+      #   "ccube_staging_default"
+      # else
+      #   "ccube_prod_default"
+      else
+        nil
+      end
+
+      tenant = subdomain.match( matcher )
+      logger.info "#{queue} - #{tenant} : receive '#{tag["event"]}' with '#{tag["object"]}'"
+
+      if tenant && queue
+        Shoryuken::Client.queues(queue).send_message({
+          message_body: tag,
+          message_attributes: {
+            tenant: {
+              string_value: tenant[1],
+              data_type: "String"
+            }
+          }
+        })
+      end
+    rescue => e
+      Rollbar.error(e)
+      return
+    end
+
   end
 
   def postmark(event)
